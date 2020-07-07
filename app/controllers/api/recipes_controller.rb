8cc89e374784301cb4990d03dc2e7086be6e3842
“irobetson” <ianrobertson427@gmail.com>
2020-07-06 20:38:58 -0500

    Fixes w routes

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,19 +1,29 @@
 class Api::ProductsController < ApplicationController
   
   @recipe = Recipe.find_by(id: params[:id])
-  render 'show.json.jb'
+  render 'create.json.jb'
 end
+
+#def is_discounted
+#if price < 10
+#end
+
+#end
 
 def create
   # make a new recipe in the db
-  @recipe = Recipe.new(      
+  @recipe = Recipe.new(   
+
     chef: params[:chef],
     title: params[:title],
     ingredients: params[:ingredients],
     prep_time: params[:prep_time],
     directions: params[:directions],
-    image_url: params[:image_url]
+    image_url: params[:image_url],
+    price: params[:price]
+
   )
+  
   @recipe.save
-  render 'show.json.jb'
+  render 'create.json.jb'
 end
