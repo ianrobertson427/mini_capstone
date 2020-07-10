6b6da3c736c917d4e70d1d4cc97426888ccd316c
“irobetson” <ianrobertson427@gmail.com>
2020-07-09 19:50:53 -0500

    syntax errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,27 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 10..500 }
+
+  # def hello 
+  #   "say hello"  
+  # end
+
+  # is_discounted?
+  def is_discounted?
+    price < 10
+  end
+
+  # tax
+  def tax
+    price * 0.09
+  end
+  # total
+
+  def total
+    price + tax
+  end
 end
