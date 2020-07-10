50345e131a2979704745daf7efe786be30333b53
“irobetson” <ianrobertson427@gmail.com>
2020-07-09 19:48:22 -0500

    wrong controller fix

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,29 +1,23 @@
+
 class Api::ProductsController < ApplicationController
-  
-  @recipe = Recipe.find_by(id: params[:id])
-  render 'create.json.jb'
-end
+  def index
+    @products = Product.all
+    render 'index.json.jb'
+  end
 
-#def is_discounted
-#if price < 10
-#end
-
-#end
-
-def create
-  # make a new recipe in the db
-  @recipe = Recipe.new(   
-
-    chef: params[:chef],
-    title: params[:title],
-    ingredients: params[:ingredients],
-    prep_time: params[:prep_time],
-    directions: params[:directions],
-    image_url: params[:image_url],
-    price: params[:price]
-
-  )
-  
-  @recipe.save
-  render 'create.json.jb'
-end
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    if @product.save
+      # happy path
+      render 'show.json.jb'
+    else
+      # sad path
+      # render 'errors.json.jb', status: :unprocessable_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
-class Api::ProductsController < ApplicationController
-  def index
-    @products = Product.all
-    render 'index.json.jb'
-  end
-
-  def create
-    @product = Product.new(
-      name: params[:name],
-      price: params[:price],
-      image_url: params[:image_url],
-      description: params[:description],
-    )
-    if @product.save
-      # happy path
-      render 'show.json.jb'
-    else
-      # sad path
-      # render 'errors.json.jb', status: :unprocessable_entity
-      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
-    end
-  end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+end
